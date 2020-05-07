37209dfe5283a72a4d6e70862cd62f9d028eb0e8
Mike O'Donnell <mikeod38@gmail.com>
2020-05-07 17:52:16 -0400

    made a new package

---
+++ R/test_function.R
@@ -0,0 +1,13 @@
+#' test_function
+#'
+#' This is my description
+#'
+#' @param x the first number
+#' @param y the second number
+#'
+#' @export
+#' @example test_function(2,3)
+#'
+test_function <- function(x,y) {
+  x + y
+}
